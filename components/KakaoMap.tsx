a58371c3ce85eff05f2653e28c3d8f14efa2fd02
yedio <yediobjet@gmail.com>
2025-02-09 16:38:42 +0900

    Add: 카카오맵 돌아가기 버튼 추가

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { cls } from "@libs/client/Utility";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { CommonImage } from "./image/CommonImage";
 
 declare global {
   interface Window {
@@ -13,6 +14,31 @@ interface KakaoMapProps {
   providedStyle?: string;
 }
 export default function KakaoMap({ address, providedStyle }: KakaoMapProps) {
+  const mapInit = () => {
+    const mapContainer = document.getElementById("map");
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(0, 0), // 지도의 중심좌표 (경도 & 위도)
+      level: 3, // 지도의 확대 레벨
+    };
+    const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+    // 주소로 좌표 검색
+    const geocoder = new window.kakao.maps.services.Geocoder();
+
+    geocoder.addressSearch(address, function (result: any, status: any) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+        map.setCenter(coords);
+
+        // 마커 표시
+        const marker = new window.kakao.maps.Marker({
+          map: map,
+          position: coords,
+        });
+        marker.setMap(map);
+      }
+    });
+  };
   useEffect(() => {
     const mapScript = document.createElement("script");
 
@@ -23,39 +49,23 @@ export default function KakaoMap({ address, providedStyle }: KakaoMapProps) {
 
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById("map");
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(0, 0), // 지도의 중심좌표 (경도 & 위도)
-          level: 3, // 지도의 확대 레벨
-        };
-        const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-        // 주소로 좌표 검색
-        const geocoder = new window.kakao.maps.services.Geocoder();
-
-        geocoder.addressSearch(address, function (result: any, status: any) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            const coords = new window.kakao.maps.LatLng(
-              result[0].y,
-              result[0].x
-            );
-            map.setCenter(coords);
-
-            // 마커 표시
-            const marker = new window.kakao.maps.Marker({
-              map: map,
-              position: coords,
-            });
-            marker.setMap(map);
-          }
-        });
-
-        // 추가적인 옵션 기능 (줌 & 지도타입)
+        mapInit();
       });
     };
 
     mapScript.addEventListener("load", onLoadKakaoMap);
   }, []);
 
-  return <div id="map" className={cls("w-full h-[250px] bg-grey-40")} />;
+  // return <div id="map" className={cls("w-full h-[250px] bg-grey-40")} />;
+  return (
+    <div className={cls("relative w-full h-[250px] bg-grey-40")}>
+      <div id="map" className="w-full h-full" />
+      <div
+        className="cursor-pointer flex items-center justify-center absolute bottom-2 right-2 w-8 h-8 rounded-full bg-main-color text-white z-10"
+        onClick={mapInit}
+      >
+        <CommonImage src="/img/icons/mapturn.svg" width={20} />
+      </div>
+    </div>
+  );
 }
